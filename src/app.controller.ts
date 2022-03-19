931ce52851d15977c00da060c6734820b5f1880f
Afonso Barracha <barracha.afonso@gmail.com>
2022-03-19 14:18:02 +0000

    add default route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,6 @@ import { Public } from './auth/decorators/public.decorator';
 @Controller()
 export class AppController {
   constructor(private readonly configService: ConfigService) {}
-  private readonly port = this.configService.get<number>('port');
-
   @Public()
   @Get('/favicon.ico')
   public sendFavicon(@Res() res: FastifyReply) {
@@ -17,6 +15,6 @@ export class AppController {
   @Public()
   @Get()
   public getServerRunning() {
-    return `server running on port ${this.port}`;
+    return `server running on port 5000`;
   }
 }
